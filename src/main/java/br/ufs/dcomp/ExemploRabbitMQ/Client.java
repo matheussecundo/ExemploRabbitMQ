5848e9d080eefabbc1437aa4036dccc77296c910
matheussecundo <matheus.gois@dcomp.ufs.br>
2020-12-07 19:12:18 -0300

    add proto

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Client.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Client.java
@@ -1,13 +1,24 @@
 package br.ufs.dcomp.ExemploRabbitMQ;
 
-import java.util.Scanner;
-import java.util.ArrayList;
 // mvn exec:java -D exec.mainClass=br.ufs.dcomp.ExemploRabbitMQ.Client
 // mvn compile assembly:single
 // java -jar target/Client.jar
 // ssh -i rabbit-key.pem ubuntu@18.209.225.188
 
+import java.util.Scanner;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import br.ufs.dcomp.RabbitChat.Proto;
+
 public class Client {
+
+    static ArrayList<Proto.Message> messages;
+    static ZoneOffset zoneOffset = ZoneOffset.of(ZoneId.systemDefault().getId());
+    static RabbitChat chat;
 
     public static void main(String[] argv) throws Exception {
         Scanner keyboard = new Scanner(System.in);
@@ -15,26 +26,14 @@ public class Client {
         System.out.print("User: ");
         String username = keyboard.nextLine();
 
-        RabbitChat chat = new RabbitChat(
+        chat = new RabbitChat(
                 RabbitChat.makeConnection("sieghart", "rabbit", "ec2-52-87-217-20.compute-1.amazonaws.com", "/"),
                 username);
 
-        chat.onNewMessage((ArrayList<RabbitChat.ChatMessage> messages) -> {
-            try {
-                if (System.getProperty("os.name").contains("Windows")) {
-                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-                } else {
-                    Runtime.getRuntime().exec("clear");
-                }
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+        chat.onNewMessage((Proto.Message message) -> {
+            messages.add(message);
 
-            for (RabbitChat.ChatMessage message : messages) {
-                System.out.println(message);
-            }
-
-            System.out.print("@" + chat.getQueueName() + ">>");
+            printPrompt();
         });
 
         while (true) {
@@ -61,5 +60,40 @@ public class Client {
         }
 
         keyboard.close();
+    }
+
+    static private void printPrompt() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        for (Proto.Message x : messages) {                
+            LocalDateTime dateTime = LocalDateTime.ofEpochSecond(
+                x.getTimestamp().getSeconds(),
+                x.getTimestamp().getNanos(),
+                zoneOffset
+            );
+
+            System.out.println(
+                "("
+                +
+                dateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+                +
+                " as "
+                +
+                dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
+                +
+                ")"
+                + " " + x.getEmiter() + " diz " + x.getContent().getBody()
+            );
+        }
+
+        System.out.print("@" + chat.getQueueName() + ">>");
     }
 }
--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/RabbitChat.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/RabbitChat.java
@@ -1,14 +1,15 @@
 package br.ufs.dcomp.ExemploRabbitMQ;
 
-import com.google.gson.Gson;
+// protoc --java_out=src/main/java/ src/main/proto/*.proto
+
 import com.rabbitmq.client.*;
+import br.ufs.dcomp.RabbitChat.Proto;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Timestamp;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.concurrent.TimeoutException;
-import java.util.ArrayList;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class RabbitChat
 {
@@ -16,14 +17,12 @@ public class RabbitChat
     private Channel _channel;
     private String _username;
     private String _queueName;
-    private ArrayList<ChatMessage> _messages;
-    private java.util.function.Consumer<ArrayList<ChatMessage>> _newMessageCallback;
+    private java.util.function.Consumer<Proto.Message> _newMessageCallback;
 
     public RabbitChat(Connection connection, String username)
     {
         _connection = connection;
         _username = username;
-        _messages = new ArrayList<>();
     }
     
     public static Connection makeConnection(String username, String password, String host, String virtualHost) throws IOException, TimeoutException
@@ -60,11 +59,23 @@ public class RabbitChat
 
     public void sendMessage(String message) throws IOException
     {
-        Gson gson = new Gson();
+        Instant time = Instant.now();
+        
+        Proto.Message toSend = Proto.Message.newBuilder()
+            .setEmiter(_username)
+            .setTimestamp(
+                Timestamp.newBuilder()
+                    .setSeconds(time.getEpochSecond())
+                    .setNanos(time.getNano())
+            )
+            .setContent(
+                Proto.Content.newBuilder()
+                    .setType("text/plain")
+                    .setBody(ByteString.copyFrom(message.getBytes()))
+            )
+            .build();
 
-        String jsonMessage = gson.toJson(new ChatMessage(_username, message));
-
-        _channel.basicPublish("", _queueName, null, jsonMessage.getBytes());
+        _channel.basicPublish("", _queueName, null, toSend.toByteArray());
     }
 
     private String watchMessages() throws IOException
@@ -73,14 +84,10 @@ public class RabbitChat
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException
             {
-                Gson gson = new Gson();
-
-                ChatMessage chatMessage = gson.fromJson(new String(body, "UTF-8"), ChatMessage.class);
-
-                _messages.add(chatMessage);
+                Proto.Message message = Proto.Message.parseFrom(body);
 
                 if (_newMessageCallback != null) {
-                    _newMessageCallback.accept(_messages);
+                    _newMessageCallback.accept(message);
                 }
             }
         };
@@ -88,7 +95,7 @@ public class RabbitChat
         return _channel.basicConsume(_queueName, true, consumer);
     }
     
-    public void onNewMessage(java.util.function.Consumer<ArrayList<ChatMessage>> newMessageCallback)
+    public void onNewMessage(java.util.function.Consumer<Proto.Message> newMessageCallback)
     {
         _newMessageCallback = newMessageCallback;
     }
@@ -101,29 +108,5 @@ public class RabbitChat
     public String getQueueName()
     {
         return _queueName;
-    }
-
-    public class ChatMessage
-    {
-        private String _username;
-        private String _message;
-        private String _datetime;
-
-        public ChatMessage(String username, String message)
-        {
-            _username = username;
-            _message = message;
-
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter dateFormater = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-            DateTimeFormatter timeFormater = DateTimeFormatter.ofPattern("HH:mm");
-
-            _datetime = "(" + now.format(dateFormater) + " ás " + now.format(timeFormater) + ")";
-        }
-
-        @Override
-        public String toString() {
-            return _datetime + " " + _username + " diz " + _message;
-        }
     }
 }
